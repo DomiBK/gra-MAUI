71081c0ccfdaa79854d26c1eebfc4975e257286f
DomiBK <148908251+DomiBK@users.noreply.github.com>
2024-03-13 10:10:06 +0100

    Create App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
